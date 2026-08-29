57bc70a20fc6c2c4e7e98f51dfbd0b85a6e43d40
kkuhsal3-max <kkuhsal3@gmail.com>
2026-08-30 02:16:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -224,11 +224,7 @@ def callback_handler(call):
         },
         "p3": { # ₹96 -> 3 Videos
             "name": "PLAN 3 PACK", "price": "96", "validity": "30 Days",
-            "desc": "✨10000+ 𝐌0𝐦&𝐒0𝐧 Videos
-              ✨ 6000+ 𝐑@𝐩€ Videos
-              ✨ New Content upload Regularly
-           📦 ✨ G0RE R@PE ✨
-             💰 Price: ₹96 | ⏳ 30 Days ",
+            "desc": "PREMIUM EXCLUSIVE ACCESS",
             "media": ["videos/video4.mp4", "videos/video5.mp4", "videos/video6.mp4"]
         },
         "p4": { # ₹155 -> 2 Videos + 1 Photo
@@ -327,4 +323,4 @@ if __name__ == '__main__':
         except Exception as e:
             logging.error(f"Polling crash prevented: {e}")
             time.sleep(3)
-        
+            
